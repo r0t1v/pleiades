b3576f9e6d7b9847e7fe2b9b581a2d20f4aee1f2
Vitor Gabriel <60289651+r0t1v@users.noreply.github.com>
2022-11-24 22:37:30 -0300

    menu system interface

--- pages/system.php
+++ pages/system.php
@@ -19,64 +19,53 @@ require __DIR__.'/../config.php';
     </head>
     <body>
         <div class="menusystemuser" align="center">
-            <img src="../assets/base/system_logo.png" alt="logosystem"/>
-            <p><?= $servername.' <span class="badge rounded-pill text-bg-warning">'.$releaseversion.'</span>'; ?></p>
-            <hr>
-            <ul>
-                <li>
-                    <a class="btn btn-secondary" href="/pleiades/pages/system.php" role="button">
-                        <span class="badge rounded-pill text-bg-light"><i class="bi bi-clipboard-data"></i>
-                        </span> Minha Dashboard
-                    </a>
-                </li>
-                <li>
-                    <a class="btn btn-secondary" href="#mytickets" role="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne">
-                        <span class="badge rounded-pill text-bg-light"><i class="bi bi-ticket-perforated"></i>
-                        </span> Meus Tickets
-                    </a>
-                    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#mytickets">
-                        <div class="accordion-body">
-                            <ul>
-                                <li id="menuaddtickt">
-                                    <a class="btn btn-secondary"  href="/" role="button"><i class="bi bi-plus-circle"></i> Criar Ticket</a>
-                                </li>
-                                <li>
-                                    <a class="btn btn-secondary" href="/" role="button"><i class="bi bi-clock-history"></i> Pendentes</a>
-                                </li>
-                                <li>
-                                    <a class="btn btn-secondary" href="/" role="button"><i class="bi bi-check-circle"></i> Finalizados</a>
-                                </li>
-                            </ul>
-                        </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-1">
+                        <a href="/pleiades/pages/system.php">
+                            <img src="../assets/base/system_logo.png" width="45px" height="45px" alt="logosystem"/>
+                        </a>
                     </div>
-                </li>
-                <li>
-                    <a class="btn btn-secondary" href="" role="button">
-                        <span class="badge rounded-pill text-bg-light"><i class="bi bi-people"></i>
-                        </span> Corporativo
+                    <div class="col-sm-1">
+                        <strong><?= $servername; ?></strong>
+                    </div>
+                    <div class="col-sm-1">
+                        <span class="badge rounded-pill text-bg-warning"><?= $releaseversion; ?></span>
+                    </div>
+                    <a class="col-sm-2" href="/pleiades/pages/system.php" role="button">
+                        <span class="badge rounded-pill text-bg-light"><i class="bi bi-clipboard-data"></i></span> Minha Dashboard
                     </a>
-                </li>
-                <li>
-                    <a class="btn btn-secondary" href="https://github.com/r0t1v/pleiades/wiki" target="blank" role="button">
-                        <span class="badge rounded-pill text-bg-light"><i class="bi bi-book"></i>
-                        </span> Wiki
+                    <a class="col-sm-2 dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="badge rounded-pill text-bg-light"><i class="bi bi-ticket-perforated"></i></span> Meus Tickets
                     </a>
-                </li>
-                <li>
-                    <a class="btn btn-secondary" href="https://github.com/r0t1v/pleiades" target="blank" role="button">
-                        <span class="badge rounded-pill text-bg-light"><i class="bi bi-github"></i>
-                        </span> Sobre
+                    <ul class="dropdown-menu" id="ticketdropdown">
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-plus-circle"></i> Abrir Ticket</a></li>
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-clock-history"></i> Pendentes</a></li>
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-check-circle"></i> Finalizados</a></li>
+                    </ul>
+                    <a class="col-sm-2" href="" role="button">
+                        <span class="badge rounded-pill text-bg-light"><i class="bi bi-people"></i></span> Corporativo
                     </a>
-                </li>
-            </ul>
+                    <div class="col-sm-3">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                    </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#"><i class="bi bi-person-fill"></i> Meu Perfil</a></li>
+                            <li><a class="dropdown-item" href="#"><i class="bi bi-key-fill"></i> Alterar senha</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="../scripts/logout.php"><i class="bi bi-door-open"></i> Deslogar</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>        
         </div>
-        <nav class="menuperfil">
-            perfil
-        </nav>
-        <a href="../scripts/logout.php">Sair</a>
         <footer align="center">
-
+            <div class="mainfoot">
+                <a href="https://github.com/r0t1v/pleiades" target="blank"><i class="bi bi-github"></i></a>
+                <a href="https://github.com/r0t1v/pleiades/wiki" target="blank"><i class="bi bi-book"></i></a>  
+            </div>
         </footer>
-        <script type="text/javascript" src="../vendor/twbs/bootstrap/dist/js/bootstrap.js"></script>
+        <script type="text/javascript" src="../vendor/twbs/bootstrap/dist/js/bootstrap.bundle.js"></script>
     </body>
 </html>
